65cbca8b81bbb78e4fa8eb40c59588ab4bd3da2f
Noah <jan.noah.sabugo@estudiantat.upc.edu>
2026-09-22 22:56:53 +0200

    s

--- abc.cc
+++ abc.cc
@@ -1,32 +1,34 @@
 #include <iostream>
 using namespace std;
-
+//PRE x 
 int main (){
-        char x;
-        cin >> x;
+        char x;//assignem x
+        cin >> x;//input x
 
-    if (x>='a' and x<='z'){
-        if  (x=='a' or x=='e' or x=='i' or x=='o' or x=='u') {
-        cout<< "lowercase" << endl;
-        cout<<"vowel"<<endl;
+    if (x>='a' and x<='z'){//escull les minuscules
+        if  (x=='a' or x=='e' or x=='i' or x=='o' or x=='u') {//escull les vocals
+        cout<< "lowercase" << endl;//imprimeix lowercase
+        cout<<"vowel"<<endl;//imprimeix vowels
         }
         else {
-            cout<< "lowercase" << endl;
-            cout<< "consonant" << endl;
+            cout<< "lowercase" << endl;//imprimeix lowercase
+            cout<< "consonant" << endl;//imprimeix consonant
             }
     }
 
     else if (x>='A' and x<='Z'){
             if  (x=='A' or x=='E' or x=='I' or x=='O' or x=='U') {
-        cout<< "uppercase" << endl;
-        cout<<"vowel"<<endl;
+        cout<< "uppercase" << endl;//imprimeix uppercase
+        cout<<"vowel"<<endl;//imprimeix vowels
         }
         else {
-            cout<< "uppercase" << endl;
-            cout<< "consonant" << endl;
+            cout<< "uppercase" << endl;//imprimeix uppercase
+            cout<< "consonant" << endl;//imprimeix consonant
             }
          
         }
 
 
 }
+//Post= { Imprimeix si x és vocal/consonant i majúscula/minúscula segons l'alfabet anglès
+    // Si x no és una lletra de l'alfabet anglès, no imprimeix res. }
